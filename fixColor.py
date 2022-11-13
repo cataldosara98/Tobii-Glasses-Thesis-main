d0fa2f2082a712f9aa9cf20e8c2e041f78bd0c62
Paolog98 <95080181+Paolog98@users.noreply.github.com>
2022-11-13 18:30:31 +0100

    Merge remote-tracking branch 'origin/master'

--- fixColor.py
+++ fixColor.py
@@ -1,6 +1,8 @@
 # Funzione per settare i colori, ha come paramentro un int
 
 import PySimpleGUI as sg
+
+
 def setColor(color):
 
     if color == 1:
@@ -21,21 +23,42 @@ def setColor(color):
 # Funzione che restituisce i colori di ogni intervallo
 def interChoose():
     # L'utente deve inserire il numero di intervalli che vuole creare
-    n = int(sg.popup_get_text("Quanti intervalli vuoi creare ?(max 5): "))
 
-    col = []                                    # Lista di colori
-    # Loop del numero di intervalli per settare il colore ad ogni intervallo
-    for i in range(n):
-        color = int(sg.popup_get_text('''Seleziona il colore per il %s° intervallo: 
+
+    n =(sg.popup_get_text("Quanti intervalli vuoi creare ?(max 5): "))
+
+
+
+    while True:
+       if n!=str(1) and n!=str(2) and n!=str(3) and n!=str(4) and n!=str(5):
+
+         sg.popup_annoying("Chiusura programma per valore non valido")
+         break
+
+       else:
+
+           col = []  # Lista di colori
+           for i in range(int(n)):
+             color =int(sg.popup_get_text('''Seleziona il colore per il %s° intervallo: 
         I colori presenti nell'elenco sono:
                 1. Red
                 2. Blue
                 3. Yellow
                 4. Green
                 5. Black
-        '''  % (i+1)))
-        col.append(setColor(color))             # Aggiungo il colore alla lista dei colori
-    return col
+        '''  % (i+1)))  # Loop del numero di intervalli per settare il colore ad ogni intervallo
+
+             if color != int(1) and color != int(2) and color != int(3) and color != int(4) and color != int(5):
+
+                   sg.popup_annoying("Vaolore non valido,chiusura del programma")
+
+             else:
+                   col.append(setColor(color))
+           return col
+
+
+
+
 
 
 # Funzione che restituisce i limiti di ogni intervallo, passo per paramentro il numero di intervalli e il tempo massimo del video
